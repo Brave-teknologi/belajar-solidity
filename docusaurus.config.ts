d8682ca1f4abe35fc83f67f85dbd4b777c27950e
Fikri <fikrizhanking@gmail.com>
2025-01-26 21:02:09 +0700

    Edit base url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
    favicon: "img/sol.png",
 
    // Set the production url of your site here
-   url: "https://materi.generasibelajar.com",
+   url: "https://ms.generasibelajar.com",
    // Set the /<baseUrl>/ pathname under which your site is served
    // For GitHub pages deployment, it is often '/<projectName>/'
-   baseUrl: "/belajar-solidity",
+   baseUrl: "/",
 
    // GitHub pages deployment config.
    // If you aren't using GitHub pages, you don't need these.
